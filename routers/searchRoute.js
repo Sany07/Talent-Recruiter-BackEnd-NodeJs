9720ed6a1f018cf2d1fc411811aef6901010774a
Sany <sany.baust@gmail.com>
2025-01-11 19:15:55 +0600

    updated some router

--- routers/searchRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const getSearchResult = require("../controllers/searchController");
-
-const router = express.Router();
-
-router
-  .get("/result", getSearchResult)
-
-module.exports = router;
